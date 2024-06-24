e21076c781d6872fbff507967855bef821e85582
Akanksha10029 <akanksharani405@gmail.com>
2024-06-24 14:09:08 +0530

    solution 5

---
+++ 02_conditionals/solution_5.py
@@ -0,0 +1,10 @@
+# 5. Weather Activity Suggestion
+# Problem: Suggest an activity based on the weather (e.g., Sunny - Go for a walk, Rainy - Read a book, Snowy - Build a snowman).
+
+weather = input('Enter Weather condition:')
+if weather == "sunny":
+    print("Go for a walk")
+elif weather == "rainy":
+    print("Read a book")
+elif weather == "snowy":
+    print("Build a snowman")
