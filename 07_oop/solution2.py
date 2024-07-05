23093f70a53a509912f1f270c8d74466082f3a3b
Akanksha10029 <akanksharani405@gmail.com>
2024-07-05 14:25:52 +0530

    solution 2 - class method and self

--- 07_oop/solution2.py
+++ 07_oop/solution2.py
@@ -2,11 +2,11 @@
 # Problem: Add a method to the Car class that displays the full name of the car (brand and model).
 
 class car:
-    def __init__(self,startName,endName):
-        self.startName = startName
-        self.endName = endName
+    def __init__(self,start_name,end_name):
+        self.start_name = start_name
+        self.end_name = end_name
 
-    def fullName(self):
-        return f"{self.startName} {self.endName}"
+    def full_name(self):
+        return f"{self.start_name} {self.end_name}"
 my_car = car("Maruti","Suzuki")
-print(my_car.fullName())
+print(my_car.full_name())
