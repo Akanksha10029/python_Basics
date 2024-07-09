6321105c37e944687f536742cf2b5359831bd4de
Akanksha10029 <akanksharani405@gmail.com>
2024-07-09 19:56:43 +0530

    sol 2 - debugging func calls

--- 08_decorators/solution2.py
+++ 08_decorators/solution2.py
@@ -4,8 +4,14 @@
 def debug(func):
     def wrapper(*args, **kwargs):
         print(func.__name__, args, kwargs)
-        args_value = ", ".join(str(arg) for arg in args)
-        kwargs_value = ", ".join(f"{key}={value}" for key, value in kwargs.items())
+        if not args:
+            args_value = "0 args"
+        else:
+            args_value = ", ".join([str(arg) for arg in args])
+        if not kwargs:
+            kwargs_value = "0 kwargs"
+        else:
+            kwargs_value = ", ".join(f"{key}={value}" for key, value in kwargs.items())
         print(f"Calling: {func.__name__} with args and kwargs({args_value}, {kwargs_value})")
         return func(*args,**kwargs)
     return wrapper
